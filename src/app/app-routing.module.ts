a51ed80d30e3af931fb097e9d62ce975f8176318
adsoftsito <adsoft@live.com.mx>
2025-03-28 16:59:45 -0600

    add  crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHeaderComponent  }  from './admin-header/admin-header.component';
+import { AdminWorkexperienceComponent  }  from './admin-workexperience/admin-workexperience.component';
+//import { AdminHeaderComponent  }  from './admin-header/admin-header.component';
+//import { AdminHeaderComponent  }  from './admin-header/admin-header.component';
+//import { AdminHeaderComponent  }  from './admin-header/admin-header.component';
+//import { AdminHeaderComponent  }  from './admin-header/admin-header.component';
+//import { AdminHeaderComponent  }  from './admin-header/admin-header.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ { path: 'header', component:  AdminHeaderComponent },
+ { path: 'workexperience', component:  AdminWorkexperienceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
